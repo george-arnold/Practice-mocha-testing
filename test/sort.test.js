fa3e0d18f7e7ad11970cb05135644b0b650839e2
George Arnold <george.arnold.88@gmail.com>
2020-01-26 15:12:07 -0500

    adding tests to learn Chai

--- test/sort.test.js
+++ test/sort.test.js
@@ -13,6 +13,7 @@ describe('sort ascending', () => {
   it('should sort negative numbers in correct order', () => {
     expect(sort([-2,-19,5])).to.deep.equal([-19,-2,5]);
   });
+  
   it('should not throw error if two values are identical', () => {
     expect(sort([0,5,5])).to.deep.equal([0,5,5]);
   });
