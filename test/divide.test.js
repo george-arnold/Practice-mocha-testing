ff48bc2b72fb736627e5e3482637841b7965e6cc
George Arnold <george.arnold.88@gmail.com>
2020-01-26 12:28:19 -0500

    corrected test fn refactored test

--- test/divide.test.js
+++ test/divide.test.js
@@ -4,26 +4,10 @@ const divide = require('../index');
 
 describe('Divide function', () => {
   it('should divide positive integers correctly', () => {
-    // define inputs
-    const a = 8,
-      b = 4,
-      expectedAnswer = 2;
-
-    // invoke the function
-    const actualAnswer = divide(a, b);
-
-    // assert that expected === actual
-    expect(actualAnswer).to.equal(expectedAnswer);
+    expect(divide(8, 4)).to.equal(2);
   });
 
   it('should throw an error when divide by zero', () => {
-    // define inputs
-    const a = 8, b = 0;
-
-    // set up the function call
-    const fn = () => { divide(a, b) };
-
-    // assert that exception is thrown
-    expect(fn).to.throw();
+    expect(() => { divide(8, 0) }).to.throw();
   });
 });
